06b72d79898636607c72f156d5b93ed5007c17ba
vlad-is-love <vladyslav.vasylevych@ucu.edu.ua>
2021-02-09 15:21:00 +0200

    initialized functions check_rows, check_columns, check_colour

---
+++ puzzle.py
@@ -0,0 +1,31 @@
+"""
+
+"""
+board = [
+ "**** ****",
+ "***1 ****",
+ "**  3****",
+ "* 4 1****",
+ "     9 5 ",
+ " 6  83  *",
+ "3   1  **",
+ "  8  2***",
+ "  2  ****"
+]
+def check_rows(board: str):
+    pass
+
+
+def check_columns(board: str):
+    pass
+
+
+def check_colour(board: str):
+    pass
+
+
+def validate_board(board: str):
+    """
+    Checks rules of the game
+    """
+    pass
